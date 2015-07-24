0e031609539b33e986878e6847544b5d8a1e7f5f
Rodrigo Capilé <rcapile@gmail.com>
2015-07-23 23:43:24 -0300

    Passei para o padrão do PSR4 + ZF2

--- tests/assets/sql/mptt.create.sql
+++
@@ -1,9 +0,0 @@
- CREATE TABLE IF NOT EXISTS `mptt`  (
-                  `id` tinyint(1) unsigned NOT NULL AUTO_INCREMENT,
-                  `name` varchar(100) NOT NULL,
-                  `parent_id` tinyint(1) unsigned,
-                  `lft` tinyint(1) unsigned,
-                  `rgt` tinyint(1) unsigned,
-                  PRIMARY KEY  (`id`)
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
